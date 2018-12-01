92e04f05c2915ba33573461498f270221302cd1d
Sempu-Mizuha <sempumizuha@yahoo.co.jp>
2018-12-01 22:15:10 +0900

    タイトルと中身分けたい。
    ここが詳細らしい。

---
+++ src/servlet/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * パッケージ.クラス名:
+ * servlet.package-info
+ */
+/**
+ * TODO 作成したクラスの説明を↓に記載 & x.xにバージョンを記載:この行は削除すること
+ * 
+ * @author Sempu-Mizuha
+ * @version 1.0
+ * @since x.x 
+ */
+package servlet;
